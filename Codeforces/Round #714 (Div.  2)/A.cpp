9c2bd711fa3ac210b609fcfc27d4f3d3cf9894d3
Kirtan Ghelani <74024307+ghelanikirtan@users.noreply.github.com>
2021-05-03 20:21:15 +0530

    Add files via upload

---
+++ Codeforces/Round #714 (Div.  2)/A.cpp
@@ -0,0 +1,20 @@
+//? Array and Peaks   https://codeforces.com/contest/1513/problem/A
+
+#include<bits/stdc++.h>
+#define lli long long int
+#define fastio ios::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+
+using namespace std;
+
+int main(){
+    //fastio
+
+    //int t;
+    //cin>>t;
+    //while(t--){
+
+        
+
+    //}
+    return 0;
+}
